5b8881ac20ae84cc3a0eef5da9a4db337a6a9471
Guy Martin <gmsoft@tuxicoman.be>
2019-04-26 12:59:53 +0200

    Add version the status api endpoint.

--- src/httpd/api_status.cpp
+++ src/httpd/api_status.cpp
@@ -2,11 +2,14 @@
 
 #include "api_status.h"
 
+#include "config.h"
+
 unsigned int api_status::call(rapidjson::Document &doc, const std::string *data) {
 
 	rapidjson::Document::AllocatorType& allocator = doc.GetAllocator();
 
 	doc.AddMember("status", MHD_HTTP_OK, allocator);
+	doc.AddMember("version", PKTADDIKT_VERSION, allocator);
 
 	return MHD_HTTP_OK;
 }

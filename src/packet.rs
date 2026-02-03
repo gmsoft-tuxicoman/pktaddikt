b5f52c2cef98be919eb21eb69e9534161ee2589a
Guy Martin <gmsoft@tuxicoman.be>
2026-02-03 09:00:58 +0400

    Move packet data into PktDataSimple.

--- src/packet.rs
+++ src/packet.rs
@@ -14,7 +14,7 @@ pub struct Packet<'a> {
     pub ts: PktTime,
     pub datalink: Protocols,
     stack: Vec<PktInfo<'a>>,
-    data: PktData<'a>
+    pub data: &'a mut dyn PktData
 }
 
 
@@ -25,27 +25,15 @@ pub struct PktInfo<'a> {
 }
 
 
-struct PktData<'a> {
-    buffers: Vec<PktBuff<'a>>,
-    length: usize,
-    read_offset: usize
-}
-
-struct PktBuff<'a> {
-    offset: usize,
-    data: &'a[u8]
-}
-
-
 impl<'a> Packet<'a> {
 
-    pub fn new(ts: PktTime, datalink: Protocols, data: &'a[u8]) -> Self {
+    pub fn new(ts: PktTime, datalink: Protocols, data: &'a mut impl PktData) -> Self {
 
         Packet {
             ts: ts,
             datalink: datalink,
             stack: Vec::with_capacity(7),
-            data: PktData::new(data)
+            data: data
         }
 
     }
@@ -72,65 +60,90 @@ impl<'a> Packet<'a> {
         self.stack.iter()
     }
 
-    pub fn remaining_len(&self) -> usize {
-        self.data.length - self.data.read_offset
-    }
-
-    pub fn skip_bytes(&mut self, size: usize) -> Result<(),()> {
-        trace!("Skipping {} bytes from pkt {:p}", size, self);
-        if self.data.length - self.data.read_offset < size {
-            self.data.read_offset = self.data.length;
-            return Err(());
-        }
-        self.data.read_offset += size;
-        return Ok(());
-    }
-
-    pub fn shrink(&mut self, new_size: usize) {
-        trace!("Shrinking data to {} (was {})", new_size, self.data.length);
-        assert!(self.data.length >= new_size, "Trying to shrink a packet with a bigger length!");
-        self.data.length = new_size;
-    }
-
-    pub fn read_bytes(&mut self, size: usize) -> Option<&[u8]> {
-
-        trace!("Reading {} bytes from pkt {:p}i (off: {}, len: {})", size, self, self.data.read_offset, self.data.length);
-
-        // FIXME handle multiple buffers
-        if self.data.length - self.data.read_offset < size {
-            return None;
-        }
-        let bytes = &self.data.buffers[0].data[self.data.read_offset..(self.data.read_offset + size)];
-        self.data.read_offset += size;
-        Some(bytes)
-
-    }
-
     pub fn read_u8(&mut self) -> Option<u8> {
-        let byte = self.read_bytes(1)?;
+        let byte = self.data.read_bytes(1)?;
         Some(byte[0])
     }
 
     pub fn read_u16(&mut self) -> Option<u16> {
-        let bytes = self.read_bytes(2)?;
+        let bytes = self.data.read_bytes(2)?;
         Some((bytes[0] as u16) << 8 | (bytes[1] as u16))
+    }
+
+    pub fn remaining_len(&self) -> usize {
+        self.data.remaining_len()
+    }
+
+    pub fn skip_bytes(&mut self, size: usize) -> Result<(),()> {
+        self.data.skip_bytes(size)
+    }
+
+    pub fn shrink(&mut self, new_size: usize) {
+        self.data.shrink(new_size)
+    }
+
+    pub fn read_bytes(&mut self, size: usize) -> Option<&[u8]> {
+        self.data.read_bytes(size)
     }
 }
 
-impl<'a> PktData<'a> {
+pub trait PktData {
 
-    fn new(data: &'a[u8]) -> Self {
-       let pkt_buff = PktBuff {
-           offset: 0,
+    fn remaining_len(&self) -> usize;
+    fn skip_bytes(&mut self, size: usize) -> Result<(),()>;
+    fn shrink(&mut self, new_size: usize);
+    fn read_bytes(&mut self, size: usize) -> Option<&[u8]>;
+}
+
+pub struct PktDataSimple<'a> {
+    read_offset: usize,
+    data: &'a[u8]
+}
+
+impl<'a> PktDataSimple<'a> {
+
+    pub fn new(data: &'a[u8]) -> Self {
+        PktDataSimple {
+           read_offset: 0,
            data: data
-        };
-
-        PktData {
-            length: data.len(),
-            read_offset: 0,
-            buffers: vec![pkt_buff]
         }
     }
+}
+
+impl PktData for PktDataSimple<'_> {
+
+    fn remaining_len(&self) -> usize {
+        self.data.len() - self.read_offset
+    }
+
+    fn skip_bytes(&mut self, size: usize) -> Result<(),()> {
+        trace!("Skipping {} bytes from pkt {:p}", size, self);
+        if self.data.len() - self.read_offset < size {
+            self.read_offset = self.data.len();
+            return Err(());
+        }
+        self.read_offset += size;
+        return Ok(());
+    }
+
+    fn shrink(&mut self, new_size: usize) {
+        trace!("Shrinking data to {} (was {})", new_size, self.data.len());
+        assert!(self.data.len() >= new_size, "Trying to shrink a packet with a bigger length!");
+        self.data = &self.data[0..new_size];
+    }
+
+    fn read_bytes(&mut self, size: usize) -> Option<&[u8]> {
+
+        trace!("Reading {} bytes from pkt {:p}i (off: {}, len: {})", size, self, self.read_offset, self.data.len());
+        if self.read_offset + size < self.data.len() {
+            return None;
+        }
+        let bytes = &self.data[self.read_offset..(self.read_offset + size)];
+        self.read_offset += size;
+        Some(bytes)
+
+    }
+
 }
 
 impl<'a> PktInfo<'a> {

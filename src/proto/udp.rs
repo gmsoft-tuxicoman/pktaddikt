6a5b0fc9edb4c6a86d8a97fd37a8c32823eaa18d
Guy Martin <gmsoft@tuxicoman.be>
2026-02-05 13:56:19 +0400

    Check validity of len field in UDP packet.

--- src/proto/udp.rs
+++ src/proto/udp.rs
@@ -34,6 +34,10 @@ impl ProtoProcessor for ProtoUdp {
         let dport : u16 = (hdr[2] as u16) << 8 | (hdr[3] as u16);
         let len : u16 = (hdr[4] as u16) << 8 | (hdr[5] as u16);
 
+        if len < 9 { // Assume at least one byte
+            return ProtoParseResult::Invalid;
+        }
+
         let plen = (len as usize) - 8;
         if plen > pkt.remaining_len() {
             // Stop processing if payload is not complete
